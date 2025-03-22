b96a9316ebc6fb80337c374612ca8c6c72764590
unknown <p.mwtr99@gmail.com>
2025-03-22 19:41:04 +0700

    fix: TellerCardBrowse dynamic in browseTeller page

--- src/app/browseTeller/page.tsx
+++ src/app/browseTeller/page.tsx
@@ -1,19 +1,38 @@
 import TellerCardBrowse from "../../components/TellerCardBrowse";
 
+const tellers = [
+  {
+    imageSrc: "/teller00.png",
+    rating: 4.8,
+    tellerName: "Golf the Teller",
+    tags: ["Tarot Reading", "Love and relationship"],
+    description:
+      "I have been practicing tarot for over 20 years and graduated from a well-known institution....",
+    reviews: 28,
+    waitTime: 3,
+    price: 200,
+    questions: 3,
+  },
+  {
+    imageSrc: "/teller00.png",
+    rating: 1.4,
+    tellerName: "Golfy the Teller",
+    tags: ["Love and relationship"],
+    description:
+      "I have been in love for over 20 years and graduated from romance novels....",
+    reviews: 2,
+    waitTime: 100,
+    price: 20,
+    questions: 5,
+  },
+];
+
 export default function BrowseTeller() {
   return (
     <div className="flex flex-col items-center overflow-y-auto justify-between">
-      <TellerCardBrowse
-        imageSrc="/teller00.png"
-        rating={4.8}
-        tellerName="Golf the Teller"
-        tags={["Tarot Reading", "Love and relationship"]}
-        description="I have been practicing tarot for over 20 years and graduated from a well-known institution...."
-        reviews={28}
-        waitTime={3}
-        price={200}
-        questions={3}
-      />
+      {tellers.map((teller, index) => (
+        <TellerCardBrowse key={index} {...teller} />
+      ))}
     </div>
   );
 }
