3a1fa0dbe69f5355e913d4e9bd640dd30cea7112
Mekk :) <meaww@mekk-mac.local>
2025-03-31 14:58:58 +0700

    Delete choose payment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,15 @@ import { IoCompass } from "react-icons/io5";
 import { FaUser } from "react-icons/fa6";
 
 
-
-
 const Navbar = () => {
     return (
         <nav className="fixed bottom-0 left-0 w-full h-[76px] bg-blue01 text-white flex items-center justify-center z-50">
             <div className="flex flex-row items-center justify-center gap-24">
+              <span>
                 <MdHomeFilled className="fill-white" size={34} />
                 <IoCompass className="fill-white" size={34} />
                 <FaUser className="fill-white" size={26} />
+                </span>
             </div>
 
         </nav>
