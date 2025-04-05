347c9416cec2ff9163c5f70e999f6732705312b7
unknown <p.mwtr99@gmail.com>
2025-04-06 00:02:41 +0700

    fix: header and navbar inside container

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,16 @@ import { MdHomeFilled } from "react-icons/md";
 import { IoCompass } from "react-icons/io5";
 import { FaUser } from "react-icons/fa6";
 
-
 const Navbar = () => {
-    return (
-        <nav className="fixed bottom-0 left-0 w-full h-[76px] bg-blue01 text-white flex items-center justify-center z-50">
-            <div className="flex flex-row items-center justify-center gap-24">
-                <MdHomeFilled className="fill-white" size={34} />
-                <IoCompass className="fill-white" size={34} />
-                <FaUser className="fill-white" size={26} />
-            </div>
-
-        </nav>
-    )
-}
-
+  return (
+    <nav className="absolute bottom-0 left-0 w-full h-[76px] bg-blue01 text-white flex items-center justify-center z-50">
+      <div className="flex flex-row items-center justify-center gap-24">
+        <MdHomeFilled className="fill-white" size={34} />
+        <IoCompass className="fill-white" size={34} />
+        <FaUser className="fill-white" size={26} />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
