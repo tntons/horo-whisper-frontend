5348eff11efd6c7c53c2c31253f7c2a43a026b2e
Saharat Nuamchit <saharat.nuamchit@gmail.com>
2025-04-23 09:17:00 +0700

    chore: meta data

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -12,6 +12,7 @@ export default function IndexPage() {
                 name="apple-mobile-web-app-status-bar-style"
                 content="black-translucent"
             />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
         </Head>
     );
 }
